79084cbdaa68773b1aaa790f4d527fb6c5a0cc2b
Remy Jourde <remy.jourde@gmail.com>
2014-08-11 14:22:02 +0200

    Creation of application as Tabbed Application

---
+++ Gonawin/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Gonawin
+//
+//  Created by Remy JOURDE on 11/08/2014.
+//  Copyright (c) 2014 Taironas. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
