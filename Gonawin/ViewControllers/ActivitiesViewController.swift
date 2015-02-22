b4da7b53d684c73ff1089fe9105bf8ac672d77e2
Remy Jourde <remy.jourde@gmail.com>
2015-02-22 17:52:28 +0100

    create ActivitesViewController

---
+++ Gonawin/ViewControllers/ActivitiesViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ActivitiesViewController
+//  Gonawin
+//
+//  Created by Remy JOURDE on 11/08/2014.
+//  Copyright (c) 2014 Taironas. All rights reserved.
+//
+
+import UIKit
+
+class ActivitiesViewController: UITableViewController {
+    
+}
+
